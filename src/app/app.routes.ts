f1edaa55f4fe1a2091bdc7eaeb1aa2f559688a31
Jhoselin <jhoselin.cas03@gmail.com>
2025-09-22 14:40:14 -0400

    feat(home): integrate template layout into Home

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,11 @@
-import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+import { Routes } from '@angular/router';
+import { App } from './app';
+
+export const routes: Routes = [
+    { path: '', component: App }, // Esta es la ruta por defecto
+    { path: 'portfolio', component: App }, // Enlaza a la sección de portafolio
+    { path: 'about', component: App }, // Enlaza a la sección de "acerca de"
+    { path: 'contact', component: App }, // Enlaza a la sección de contacto
+    // Puedes agregar más rutas si lo necesitas
+];
